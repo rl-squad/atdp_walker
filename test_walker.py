e6d0fc6b3dff809adc32dcf41a334f6752e12715
James Sue-Ling <james_sueling@hotmail.co.uk>
2025-03-26 10:42:20 +0000

    switch alive_progress to tqdm

--- test_walker.py
+++ test_walker.py
@@ -1,11 +1,11 @@
 import time
 import gymnasium as gym
-from alive_progress import alive_it
+from tqdm import tqdm
 
 env = gym.make("Walker2d-v5", render_mode="rgb_array")
 observation, _ = env.reset()
 
-for _ in alive_it(range(1000)):
+for _ in tqdm(range(1000)):
     time.sleep(1) # Just to test the progress bar works in container
     action = env.action_space.sample()  # Random policy
     observation, reward, terminated, truncated, info = env.step(action)
