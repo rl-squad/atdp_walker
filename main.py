cd19dc80f5f2dc2efd2e04d6c8e1a907cb2e4782
cmc228 <cmc228@bath.ac.uk>
2025-04-01 06:37:15 +0100

    Refactor main.py to accept --lib in addition to --job-id
    Previously:
        python main.py train --job-id JOBID
    
    Now:
        python main.py train --lib stable --job-id JOBID
    
    This allows selecting different training methods (e.g., stable, cleanrl) using a single entry point.

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import argparse
-import train
+from train import train_stable, train_cleanrl
 
 def main():
-    parser = argparse.ArgumentParser(description="ATDP Walker")
+    # Set up argument parsing
+    parser = argparse.ArgumentParser(description="ATDP Walker PPO Trainer")
     subparsers = parser.add_subparsers(dest="command", required=True)
-    
-    train_parser = subparsers.add_parser('train', help="Train the model")
-    train_parser.add_argument('--job-id', type=str, required=True, help="Job ID for tracking this training run")
-    train_parser.add_argument('--job-description', type=str, required=False, help="A description of this training run")
+
+    # Subparser for the 'train' command
+    train_parser = subparsers.add_parser("train", help="Train the model")
+    train_parser.add_argument("--lib", type=str, choices=["stable", "cleanrl"], required=True, help="Library to use")
+    train_parser.add_argument("--job-id", type=str, required=True, help="Job ID for this run")
+    train_parser.add_argument("--job-description", type=str, default="", help="Optional description of the training run")
 
     args = parser.parse_args()
-    
-    if args.command == "train":
-        train.train(args.job_id, args.job_description)
 
+    # Call the appropriate training function
+    if args.command == "train":
+        if args.lib == "stable":
+            train_stable(args.job_id, args.job_description)
+        elif args.lib == "cleanrl":
+            train_cleanrl(args.job_id, args.job_description)
+
+# Run the main function if the script is executed
 if __name__ == "__main__":
     main()
