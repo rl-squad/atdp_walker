feb4dede1364c2b099d9b3f21073f6ad728d77eb
cmc228 <cmc228@bath.ac.uk>
2025-04-01 07:16:05 +0100

    modifed main to included all the classes to test the different dependecies that apply PPO and apply a map function to apply the correct class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
-from train import StableTrainer  # CleanRLTrainer can be added later
+from train import StableTrainer, CleanRLTrainer, RLlibTrainer, GarageTrainer, CustomTrainer, HuggingFaceTrainer
+
 
 
 def main():
@@ -9,17 +10,29 @@ def main():
 
     # Subparser for the 'train' command
     train_parser = subparsers.add_parser("train", help="Train the model")
-    train_parser.add_argument("--lib", type=str, choices=["stable"], required=True, help="Library to use")
+    train_parser.add_argument("--lib", type=str,
+                              choices=["stable", "cleanrl", "rllib", "garage", "custom", "hf_sb3"],
+                              required=True, help="Library to use")
     train_parser.add_argument("--job-id", type=str, required=True, help="Job ID for this run")
     train_parser.add_argument("--job-description", type=str, default="", help="Optional description of the training run")
 
     args = parser.parse_args()
 
-    # Instantiate and run the appropriate trainer
+    # Map string to the correct trainer class
+    trainer_map = {
+        "stable": StableTrainer,
+        "cleanrl": CleanRLTrainer,
+        "rllib": RLlibTrainer,
+        "garage": GarageTrainer,
+        "custom": CustomTrainer,
+        "hf_sb3": HuggingFaceTrainer,
+    }
+
+    # Instantiate and run the selected trainer
     if args.command == "train":
-        if args.lib == "stable":
-            trainer = StableTrainer(args.job_id, args.job_description)
-            trainer.train()
+        trainer_class = trainer_map.get(args.lib)
+        trainer = trainer_class(args.job_id, args.job_description)
+        trainer.train()
 
 
 if __name__ == "__main__":
