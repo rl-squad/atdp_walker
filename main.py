81cce67845954ffc04d3e6f802ef41d79e643f9a
cmc228 <cmc228@bath.ac.uk>
2025-04-01 07:09:49 +0100

    Update main.py to use StableTrainer class instead of function-based training

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
-from train import train_stable, train_cleanrl
+from train import StableTrainer  # CleanRLTrainer can be added later
+
 
 def main():
     # Set up argument parsing
@@ -8,19 +9,18 @@ def main():
 
     # Subparser for the 'train' command
     train_parser = subparsers.add_parser("train", help="Train the model")
-    train_parser.add_argument("--lib", type=str, choices=["stable", "cleanrl"], required=True, help="Library to use")
+    train_parser.add_argument("--lib", type=str, choices=["stable"], required=True, help="Library to use")
     train_parser.add_argument("--job-id", type=str, required=True, help="Job ID for this run")
     train_parser.add_argument("--job-description", type=str, default="", help="Optional description of the training run")
 
     args = parser.parse_args()
 
-    # Call the appropriate training function
+    # Instantiate and run the appropriate trainer
     if args.command == "train":
         if args.lib == "stable":
-            train_stable(args.job_id, args.job_description)
-        elif args.lib == "cleanrl":
-            train_cleanrl(args.job_id, args.job_description)
+            trainer = StableTrainer(args.job_id, args.job_description)
+            trainer.train()
 
-# Run the main function if the script is executed
+
 if __name__ == "__main__":
     main()
