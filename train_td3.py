22ec5545e4b27b9c2dda889996ebf83cf4e783e6
Umran Hussain <1547890+umran@users.noreply.github.com>
2025-04-04 04:00:55 +0100

    ...

--- train_td3.py
+++ train_td3.py
@@ -2,7 +2,7 @@ from algorithms.td3 import TD3
 
 def main():
     td3 = TD3(update_every=50)
-    td3.train_batch(batch_size=10)
+    td3.train_batch(batch_size=8)
 
 if __name__ == "__main__":
     main()
