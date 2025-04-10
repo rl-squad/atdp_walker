61d9dea99ab9543e9d9cc0b2c50662fe5d3958af
Jake Hulme <joh46@bath.ac.uk>
2025-04-10 12:22:06 +0100

    Benchmark changed to True

--- algorithms/td3.py
+++ algorithms/td3.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-import torch.optim as optim
+import torch.optim as optim 
 
 # local imports
 from environment import TorchEnvironment, BatchEnvironment
@@ -141,7 +141,7 @@ class TD3:
                 for i in range(self.update_every):
                     self.update(skip_policy_update=(i % self.policy_delay != 0))
     
-    def train_batch(self, num_steps=1e6, num_envs=10, benchmark=False):
+    def train_batch(self, num_steps=1e6, num_envs=10, benchmark=True):
         if self.update_every < num_envs:
             raise ValueError(f"the value of self.update_every must be greater than num_envs. self.update_every is currently set to {self.update_every}")
 
