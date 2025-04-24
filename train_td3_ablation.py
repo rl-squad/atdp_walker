0569e2fa5fe0b7aeea9e3281614212b0eaed14f1
James Sue-Ling <james_sueling@hotmail.co.uk>
2025-04-24 18:44:19 +0100

    create experiment script

--- train_td3_ablation.py
+++ train_td3_ablation.py
@@ -1,12 +1,36 @@
 from algorithms.td3_ablation import TD3_Ablation
+import os
 
 def main():
-    td3 = TD3_Ablation(
-        double_clipped_Q=False,
-        delayed_policy_updates=False,
-        target_policy_smoothing=False,
-    )
-    td3.train_batch(num_steps=1200000, benchmark=True)
+
+    # 2 ** 3 tests
+    for i in range(8):
+
+        base = "td3_ablation_"
+        
+        # 10 agents per test
+        for j in range(10):
+            
+            os.environ["OUT"] = base + str(i) + "_" + str(j)
+
+            td3_ablation = TD3_Ablation(
+                double_clipped_Q=bool(i & 4),
+                delayed_policy_updates=bool(i & 2),
+                target_policy_smoothing=bool(i & 1),
+            )
+            
+            td3_ablation.train_batch(num_steps=6000000, benchmark=True)
+
+            # print(os.getenv("OUT"), bool(i & 4), bool(i & 2), bool(i & 1))
+
+            # 000 0 DDPG
+            # 001 1 target_policy_smoothing
+            # 010 2 delayed_policy_updates
+            # 011 3 target_policy_smoothing + delayed_policy_updates
+            # 100 4 double_clipped_Q
+            # 101 5 double_clipped_Q + target_policy_smoothing
+            # 110 6 double_clipped_Q delayed_policy_updates
+            # 111 7 TD3
 
 if __name__ == "__main__":
     main()
